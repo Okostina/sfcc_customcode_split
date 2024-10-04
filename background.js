15c262a9e105bc35882b7d2b3251f44a66d7e334
okostina93 <olga@hiber.global>
2024-10-04 22:12:05 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,2 @@
+// background.js: The background script that runs continuously.
+// manifest.json: The main configuration file for the Chrome extension.
---
+++ popup/popup.js
@@ -0,0 +1,4 @@
+/* popup/: A directory containing files for the extension's popup interface.
+popup.html: The HTML structure for the popup.
+popup.js: JavaScript for popup functionality.
+popup.css: Styles for the popup. */
